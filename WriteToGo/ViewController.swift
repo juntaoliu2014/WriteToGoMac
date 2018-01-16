8d8211f2fc422ef57690f017a314aab44c6ff64a
邹雷 <julieliu@Julies-MacBook-Air.local>
2018-01-16 10:04:23 +0900

    Initial Commit

---
+++ WriteToGo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WriteToGo
+//
+//  Created by Julie Liu on 1/16/18.
+//  Copyright © 2018 Julie Liu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
