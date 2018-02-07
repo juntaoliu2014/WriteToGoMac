4e1fbde33831322c4e2e9e50e5a25249ac51677e
邹雷 <julieliu@Julies-MacBook-Air.local>
2018-02-07 13:15:35 +0900

    Now

---
+++ WriteToGo/IN2.swift
@@ -0,0 +1,26 @@
+//
+//  IN2.swift
+//  WriteToGo
+//
+//  Created by Julie Liu on 2/7/18.
+//  Copyright © 2018 Julie Liu. All rights reserved.
+//
+
+import Cocoa
+
+class InputName2: NSViewController
+{
+    
+    
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+   
+    
+    
+    
+}
