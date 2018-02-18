b649071249c06979962483d1b9ebd5f0e952d02d
邹雷 <julieliu@Julies-MacBook-Air.local>
2018-02-18 13:39:10 +0900

    Haha

---
+++ WriteToGo/MMBox1.swift
@@ -0,0 +1,27 @@
+//
+//  MMBox1.swift
+//  WriteToGo
+//
+//  Created by Julie Liu on 2/7/18.
+//  Copyright © 2018 Julie Liu. All rights reserved.
+//
+
+import Cocoa
+
+class MMBox1: NSViewController
+{
+
+    
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    
+    
+    
+    
+}
